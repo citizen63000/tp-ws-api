d86bbd5220a1b9d9ea4e6ae2d998890b10b3e3c1
lionel <ngconnect@gmail.com>
2026-01-20 15:25:24 +0100

    sf6 migration (wip)

--- config/bundles.php
+++ config/bundles.php
@@ -17,5 +17,4 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
